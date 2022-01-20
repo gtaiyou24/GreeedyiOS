06809860d1683d9a5983d2272fc2cc6a0c78c860
gtaiyou24 <gtaiyou24@gmail.com>
2022-01-21 00:26:15 +0900

    Initial Commit

---
+++ GreeedyiOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GreeedyiOS
+//
+//  Created by 田村大耀 on 2022/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GreeedyiOS/GreeedyiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreeedyiOSApp.swift
+//  GreeedyiOS
+//
+//  Created by 田村大耀 on 2022/01/21.
+//
+
+import SwiftUI
+
+@main
+struct GreeedyiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
